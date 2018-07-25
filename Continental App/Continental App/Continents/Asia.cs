aa8471b0db33b7e0434d28c32d260185a9ab7387
Dominik Lincer <dominik.lincer@gmail.com>
2018-07-25 22:37:15 +0200

    first commit

---
+++ Continental App/Continental App/Continents/Asia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Continental_App.Animals;
+
+namespace Continental_App.Continents
+{
+    class Asia : Continent
+    {
+        public override void CreateContinent()
+        {
+            Animals.Add(new Panda());
+            Animals.Add(new SlowLoris());
+        }
+    }
+}
---
+++ Continental App/Continental App/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Continental_App.Continents;
+
+namespace Continental_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Continent[] continents = new Continent[2];
+
+            continents[0] = new Africa();
+            continents[1] = new Asia();
+
+            foreach (Continent continent in continents)
+            {
+                Console.WriteLine(continent.GetType().Name + ":");
+                foreach (Animal animal in continent.Animals)
+                {
+                    Console.WriteLine(" " + animal.GetType().Name);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
